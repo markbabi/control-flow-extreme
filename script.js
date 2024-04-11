30baa06559ce08e6b71c06d39040569f316fec10
Mark Babirad <mbabirad@brainstation.io>
2024-04-11 10:11:44 -0700

    initialize project

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Don't even try to control this!!");
+
+let isSale = false;
+
+if (isSale) {
+  console.log("There is a sale: BUY BUY BUY!!");
+  console.log("PLEASE!!");
+}
+
+console.log("Some more code");
